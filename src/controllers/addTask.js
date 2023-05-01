032fcc7d29a1f77d0fed81d4a452292e14b8a19a
alaaahmade <alaaahmade66@gmail.com>
2023-05-01 20:31:48 +0300

    commit

--- src/controllers/addTask.js
+++ src/controllers/addTask.js
@@ -2,7 +2,9 @@ const addTaskQ = require('../database/query/addTask');
 
 const addTask = (req, res, next) => {
   const task = req.body
-  addTaskQ(task)
+  console.log(task);
+  if(task.task) {
+    addTaskQ(task)
     .then((result) => {
       res.status(201).json({
         error: false,
@@ -15,6 +17,13 @@ const addTask = (req, res, next) => {
         message: 'internal server error'
       });
     })
+  }else {
+    res.status(500).json({
+      error: true,
+      message: 'cant add empty string'
+    });
+  }
+  
 }
 
 module.exports = addTask;
